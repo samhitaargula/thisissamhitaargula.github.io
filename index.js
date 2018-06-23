7a51c64ec9da1a6b2d30a792e4cce2eb566951ba
samhitaargula <39863220+samhitaargula@users.noreply.github.com>
2018-06-23 16:26:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,110 @@
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+$(window).scroll(function() {
+  $(".container2").slideDown( "slow", function() {
+    // Animation complete.
+  });
+});
+
+//On scroll menu fixed to top
+	var menu = $("nav");
+	$(window).scroll(function () {
+		//more then or equals to
+		if ($(window).scrollTop() < 800) {
+			menu.removeClass('fixed-top animated slideInDown');
+
+			//less then 100px from top
+		} else {
+			menu.addClass('fixed-top animated slideInDown');
+
+		}
+	});
+
+
+(function(){
+    // Vertical Timeline - by CodyHouse.co
+	function VerticalTimeline( element ) {
+		this.element = element;
+		this.blocks = this.element.getElementsByClassName("js-cd-block");
+		this.images = this.element.getElementsByClassName("js-cd-img");
+		this.contents = this.element.getElementsByClassName("js-cd-content");
+		this.offset = 0.8;
+		this.hideBlocks();
+	};
+
+	VerticalTimeline.prototype.hideBlocks = function() {
+		//hide timeline blocks which are outside the viewport
+		if ( !"classList" in document.documentElement ) {
+			return;
+		}
+		var self = this;
+		for( var i = 0; i < this.blocks.length; i++) {
+			(function(i){
+				if( self.blocks[i].getBoundingClientRect().top > window.innerHeight*self.offset ) {
+					self.images[i].classList.add("cd-is-hidden"); 
+					self.contents[i].classList.add("cd-is-hidden"); 
+				}
+			})(i);
+		}
+	};
+
+	VerticalTimeline.prototype.showBlocks = function() {
+		if ( ! "classList" in document.documentElement ) {
+			return;
+		}
+		var self = this;
+		for( var i = 0; i < this.blocks.length; i++) {
+			(function(i){
+				if( self.contents[i].classList.contains("cd-is-hidden") && self.blocks[i].getBoundingClientRect().top <= window.innerHeight*self.offset ) {
+					// add bounce-in animation
+					self.images[i].classList.add("cd-timeline__img--bounce-in");
+					self.contents[i].classList.add("cd-timeline__content--bounce-in");
+					self.images[i].classList.remove("cd-is-hidden");
+					self.contents[i].classList.remove("cd-is-hidden");
+				}
+			})(i);
+		}
+	};
+
+	var verticalTimelines = document.getElementsByClassName("js-cd-timeline"),
+		verticalTimelinesArray = [],
+		scrolling = false;
+	if( verticalTimelines.length > 0 ) {
+		for( var i = 0; i < verticalTimelines.length; i++) {
+			(function(i){
+				verticalTimelinesArray.push(new VerticalTimeline(verticalTimelines[i]));
+			})(i);
+		}
+
+		//show timeline blocks on scrolling
+		window.addEventListener("scroll", function(event) {
+			if( !scrolling ) {
+				scrolling = true;
+				(!window.requestAnimationFrame) ? setTimeout(checkTimelineScroll, 250) : window.requestAnimationFrame(checkTimelineScroll);
+			}
+		});
+	}
+
+	function checkTimelineScroll() {
+		verticalTimelinesArray.forEach(function(timeline){
+			timeline.showBlocks();
+		});
+		scrolling = false;
+	};
+})();
